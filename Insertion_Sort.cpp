961b6263d72a1855bfb7d8c9f0523f18e3030ffa
haliss2002 <40603636+haliss2002@users.noreply.github.com>
2021-07-18 20:20:14 -0300

    Add files via upload

---
+++ Insertion_Sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void print(int t, int *a) {
+	for(int i=0; i<t; ++i) {
+		cout << "Elemento: " << i << " = " << a[i] << endl;
+	}
+	cout << "-----------------" << endl;
+}
+
+void Insertion_Sort(int t, int *a) {
+	int aux;
+    int j;
+    j=1;    
+	while (j=t-1)
+    {
+     cout << "[" << j << "]: " << a[j] << ", [" << j+1 << "]: " << a[j+1];
+			if (a[j] > a[j-1]) {
+				aux = a[j];
+				a[j] = a[j-1];
+				a[j-1] = aux;
+				cout << " - trocou!";
+                --j;
+			}
+			cout << endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+	int v[8] = {26, 49, 38, 13, 58, 87, 34, 93};
+	print(8, v);
+	Insertion_Sort(8, v);
+	print(8, v);
+	
+	return 0;
+}
